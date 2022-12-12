f1911bfaeb7e999418c6e982d6e285806c6b3afc
draiguv <dcrauv13@gmail.com>
2022-12-12 18:31:42 +0800

    Modify initial files

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const DEFAULT_GRID_SIZE = 16;
 
 const container = document.querySelector('.container');
+const btn = document.querySelector('button');
 
-let divs = '';
+function createGrids() {
+  container.innerHTML = ''; // clears grids
+  
+  const gridSize = prompt('Please set a new grid to generate from 1 to 100', DEFAULT_GRID_SIZE);
 
-for (let i = 0; i < DEFAULT_GRID_SIZE; i++) {
-  divs += '<div class="grid"></div>';
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    container.appendChild(div);
+  }
 }
-divs += '<div class="grid"></div>';
 
-container.innerHTML = divs;
+btn.addEventListener('click', createGrids);
